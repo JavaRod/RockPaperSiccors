236511b671af8d1d17845d3bc3f71defea40f2ca
Rod Musser <rmusser@yahoo.com>
2016-01-18 21:00:41 -0800

    Initial Check-In

---
+++ RockPaperSiccors/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  RockPaperSiccors
+//
+//  Created by Rodrick Musser on 1/18/16.
+//  Copyright © 2016 RodCo. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController : UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
+
